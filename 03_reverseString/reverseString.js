4b6b6577a29448a770390602461a29f9b060b61f
crL96 <190506813+crL96@users.noreply.github.com>
2024-12-18 14:12:50 +0100

    Solve excercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const array = string.split("");
+    const arrayReversed = array.reverse();
+    let reversedString = arrayReversed.join("");
+    return reversedString;
 };
 
 // Do not edit below this line
