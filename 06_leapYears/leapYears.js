e3488fee2226248efd78ff16ee391a59cd31ab34
crL96 <190506813+crL96@users.noreply.github.com>
2024-12-19 14:36:58 +0100

    Solve excercise

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,4 +1,25 @@
-const leapYears = function() {
+const leapYears = function(year) {
+    //check if year divisible by four
+    //if true, check if divisible by 100, if it is 
+    // --> not leap year, unless divisible by 400
+
+    if (year % 4 == 0) {
+        if (year % 100 == 0) {
+            if (year % 400 == 0) {
+                return true;
+            }
+            else {
+                return false;
+            }
+
+        }
+        else {
+            return true;
+        }
+    }
+    else {
+        return false;
+    }
 
 };
 
