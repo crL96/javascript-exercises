8515ad1e68a5ec9f70d98a2eac326e95e585fb1b
crL96 <190506813+crL96@users.noreply.github.com>
2024-12-19 13:41:25 +0100

    Solve excercise

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,26 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let newArray = [];
+        for (let i = 0; i < array.length; i++ ) {
+            if (!(args.includes(array[i]))) {
+                newArray.push(array[i]);
+            }
+        }
+    return newArray;
 };
+
+console.log(removeFromArray([1, 2, 3, 2], 2))
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
+
+
+
+//const removeFromArray = function(array, remove) {
+//    for (let i = 0; i < array.length; i++ ) {
+//        if (array[i] === remove) {
+//            array.splice(i, 1);
+//        }
+//    }
+//    return array;
+//};
