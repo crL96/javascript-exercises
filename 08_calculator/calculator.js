4867b9a7eaa9ff0f81d625c0a22c76762f184224
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-07 16:54:37 +0100

    Solve excercise

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a, b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(arr) {
+	return arr.reduce(function(total, item) {
+    return total + item;
+  }, 0);
 };
 
-const multiply = function() {
 
+const multiply = function(arr) {
+  return arr.reduce(function(total, item) {
+    return total * item;
+  }, 1);
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	let total = 1;
+  for (let i = 0; i < b; i++) {
+    total *= a;
+  }
+  return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+    let fact = 1;
+    for (let i = 1; i <= a; i++) {
+      fact *= i;
+    }
+    return fact;
 };
 
 // Do not edit below this line
