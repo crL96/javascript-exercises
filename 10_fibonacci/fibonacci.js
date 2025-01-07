777a563393a0f0e217c83298cc649aa35fea1fd6
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-07 17:45:07 +0100

    Solve excercise

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,23 @@
-const fibonacci = function() {
+const fibonacci = function(a) {
+    a = Number(a);
+    if (a < 0) {
+        return "OOPS";
+    }
+    else if (a == 0) {
+        return 0;
+    }
+    else {
+        let fibo = [1, 1]
+        for (let i = 2; i < a; i++) {
+            nextNum = fibo[(i-2)] + fibo[(i-1)];
+            fibo.push(nextNum);
+        }
+        return fibo[(a-1)];
+}
 
 };
+
+console.log(fibonacci(6));
 
 // Do not edit below this line
 module.exports = fibonacci;
