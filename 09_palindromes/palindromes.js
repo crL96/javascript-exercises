13aea1b38238ad7170914caa3e3389786c52ce35
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-07 17:17:26 +0100

    Solve excercise

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,17 @@
-const palindromes = function () {
+const palindromes = function (str) {
+    const strLower = str.toLowerCase();
 
+    //filter to only include alphanumerical
+    const strFiltered = strLower
+        .split("")
+        .filter(character => "abcdefghijklmnopqrstuvwxyz0123456789".includes(character))
+        .join("");
+
+    const strReverse = strFiltered.split("").reverse().join("");
+    return (strFiltered === strReverse);
 };
+
+console.log(palindromes("bat"));
 
 // Do not edit below this line
 module.exports = palindromes;
