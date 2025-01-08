ac59cf2ac668e6b2ec0f1c95dc6063e9428480f5
crL96 <190506813+crL96@users.noreply.github.com>
2025-01-08 14:52:41 +0100

    Solve exercise

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,18 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    arrSorted = arr.sort(function(a, b) {
+        const currentDate = new Date();
+        if (a.yearOfDeath == undefined) {
+            a.yearOfDeath = currentDate.getFullYear();
+        }
+        if (b.yearOfDeath == undefined) {
+            b.yearOfDeath = currentDate.getFullYear();
+        }
+        
+        const aAge = a.yearOfDeath - a.yearOfBirth;
+        const bAge = b.yearOfDeath - b.yearOfBirth;
+        return bAge - aAge;
+    });
+    return arrSorted[0];
 };
 
 // Do not edit below this line
